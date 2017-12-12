e8b686980caa6bbcc99a1f5eda9509f9a461fc20
varnitjain <varnitj21@gmail.com>
2017-12-12 16:48:58 +0530

    subject name in recycler

--- app/src/main/java/com/example/hp/incident_capture/incidence_register.java
+++ app/src/main/java/com/example/hp/incident_capture/incidence_register.java
@@ -8,26 +8,58 @@ import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 import android.widget.VideoView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+import static android.app.Activity.RESULT_OK;
 import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO;
 import static com.example.hp.incident_capture.R.id.imgView;
-import static com.example.hp.incident_capture.R.id.videoView;
+//import static com.example.hp.incident_capture.R.id.videoView;
+
+
 
 public class incidence_register extends AppCompatActivity {
-    String[] list ={"Fire","Road Accident","Health Problem","Robbery"};
+    String[] list = {"Fire", "Road Accident", "Health Problem", "Robbery"};
+    @InjectView(R.id.input_subject)
+    EditText _subjectText;
+    @InjectView(R.id.input_address)
+    EditText _addressText;
+    @InjectView(R.id.input_city)
+    EditText _cityText;
+    @InjectView(R.id.input_pin_code)
+    EditText _pin_codeText;
+    @InjectView(R.id.input_description)
+    EditText _descriptionText;
+    String url=null;
 
+    DatabaseReference mdatabaseref = FirebaseDatabase.getInstance().getReference("Reports");
 
+    final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+    String userId = "";
 
     private static int RESULT_LOAD_IMG = 1;
     String imgDecodableString;
@@ -46,7 +78,7 @@ public class incidence_register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_incidence_register);
 
-
+        ButterKnife.inject(this);
 /*
         //getting the instance of Spinner and applying
         Spinner spin=(Spinner)findViewById(R.id.spinner_ir);
@@ -56,17 +88,72 @@ public class incidence_register extends AppCompatActivity {
         aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spin.setAdapter(aa);
 */
+        if (user != null)
+
+        {
+            userId = user.getUid();
+        }
+        final DatabaseReference report = mdatabaseref.child(userId).getRef();
+
+
+        mdatabaseref.child(userId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                //Personaldetails value = dataSnapshot.getValue(Personaldetails.class);
+
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                //Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
         Button report_incidence =(Button)findViewById(R.id.btn_report_incidence);
         report_incidence.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
+                if (_subjectText == null||url==null) {
+                    Log.e("123", null);
+                    return;
+                }
+                String subject = _subjectText.getText().toString();
+                String address = _addressText.getText().toString();
+                String city = _cityText.getText().toString();
+                String pin_code = _pin_codeText.getText().toString();
+                String description = _descriptionText.getText().toString();
+                report.child(subject).setValue(subject);
+                report.child(subject).child("Subject").setValue(subject);
+                report.child(subject).child("Address").setValue(address);
+                report.child(subject).child("City").setValue(city);
+                report.child(subject).child("Pin-code").setValue(pin_code);
+                report.child(subject).child("Description").setValue(description);
+                report.child(subject).child("ImageURL").setValue(url);
+
+
+
+                setResult(RESULT_OK, null);
+                finish();
+                Toast.makeText(getApplicationContext(), "Report Submitted", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(getApplicationContext(), Reporter.class);
                 startActivity(intent);
             }
         });
     }
 
+    public String encodeBitmapAndSaveToFirebase(Bitmap bitmap) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        String imageEncoded = Base64.encodeToString(baos.toByteArray(), Base64.DEFAULT);
+
+        return imageEncoded;
+    }
 
 
     public void loadImagefromCamera(View view) {
@@ -104,6 +191,7 @@ public class incidence_register extends AppCompatActivity {
 
 
 
+
 //        Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
 //
 //        //fileUri = getOutputMediaFileUri(MEDIA_TYPE_VIDEO);
@@ -119,6 +207,8 @@ public class incidence_register extends AppCompatActivity {
 
     }
 
+
+    final DatabaseReference report = mdatabaseref.child(userId).getRef();
 
 
 
@@ -136,6 +226,7 @@ public class incidence_register extends AppCompatActivity {
             ImageView imgView = (ImageView) findViewById(R.id.imgView);
 
             imgView.setImageBitmap(bitmap);
+            url=encodeBitmapAndSaveToFirebase(bitmap);
         }
 
 
@@ -143,9 +234,13 @@ public class incidence_register extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
 
                 Uri videoUri = data.getData();
-                VideoView videoView = (VideoView) findViewById(R.id.videoView);
-                videoView.setVideoURI(videoUri);
+               //VideoView videoView = (VideoView) findViewById(R.id.videoView);
+                //videoView.setVideoURI(videoUri);
+                String userUid = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
+                //videoRef = storageRef.child("/videos/" + userUid );
+                //TODO: save the video in the db
+                //uploadData(selectedVideoUri);
 
                 // video successfully recorded
                 // preview the recorded video
@@ -202,9 +297,10 @@ public class incidence_register extends AppCompatActivity {
 
                 // Set the Image in ImageView after decoding the String
 
-                imgView.setImageBitmap(BitmapFactory
+                imgView.setImageBitmap(BitmapFactory.decodeFile(imgDecodableString));
 
-                        .decodeFile(imgDecodableString));
+                url=encodeBitmapAndSaveToFirebase(BitmapFactory.decodeFile(imgDecodableString));
+
 
 
             }
